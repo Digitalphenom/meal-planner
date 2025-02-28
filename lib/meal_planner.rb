b09ab331dadad823299a074eb6c1f5b3a94cb46c
Sedrick <digitalphenom0@gmail.com>
2025-02-27 16:32:05 -0800

    added stylesheet

--- lib/meal_planner.rb
+++ lib/meal_planner.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'erubi'
 require 'sinatra/reloader'
 
+set :public_folder, File.expand_path('../public', __dir__)
 set :views, File.expand_path('../views', __dir__)
 
 configure do
