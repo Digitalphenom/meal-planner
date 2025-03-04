afceef46c058d7834bbfd80d40916797b76b1c9e
Sedrick <digitalphenom0@gmail.com>
2025-03-03 16:52:26 -0800

    added 12 meal input limit

--- lib/meal_planner.rb
+++ lib/meal_planner.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'pry-byebug'
 require 'sinatra'
 require 'erubi'
 require 'sinatra/reloader' if development?
@@ -16,7 +16,11 @@ configure do
 end
 
 def valid_input?(input)
-  !input.to_i.zero?
+  if env["REQUEST_PATH"] == '/enter-meals'
+    !input.to_i.zero? && input.to_i.size < 13
+  else
+    !input.to_i.zero?
+  end
 end
 
 #◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
@@ -34,7 +38,7 @@ get '/enter-calories' do
 end
 
 get '/enter-meals' do
-  @placeholder_name = 'Enter your meal count'
+  @placeholder_name = 'Enter your meal count (1-12)'
   erb :"layout.html"
 end
 
@@ -60,7 +64,7 @@ post '/enter-meals' do
     session['user_meals'] = params[@input_name]
     redirect 'choose-macros'
   else
-    @placeholder_name = 'Enter a numeric value'
+    @placeholder_name = 'Enter a numeric value between 1-12'
     @add_inline = true
     erb :"layout.html"
   end
