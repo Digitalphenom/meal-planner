0aef07afde2e0ef34a16a76e514a3e8a90249a50
Sedrick <digitalphenom0@gmail.com>
2025-03-09 16:28:32 -0700

    added macro nutrient logic for protein, car and fat
    inserted macro nutrient data into meal plan view

--- lib/meal_planner.rb
+++ lib/meal_planner.rb
@@ -8,6 +8,8 @@ require 'rack-livereload'
 
 set :public_folder, File.expand_path('../public', __dir__)
 set :views, File.expand_path('../views', __dir__)
+MACRO = [:protein, :carb, :fat]
+CALORIES_PER_GRAM = {protein: 4, carb: 4, fat: 9 }
 
 configure do
   use Rack::LiveReload
@@ -40,9 +42,10 @@ end
 #◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
 
 get '/' do
+  session[:user_one] = {}
   erb :"home.html", layout: :"layout.html"
 end
-
+{ "user_one"=>{:calories => 100, }}
 get '/enter-calories' do
   erb :"calories.html", layout: :"layout.html"
 end
@@ -55,11 +58,35 @@ get '/choose-macros' do
   erb :"macros.html", layout: :"layout.html"
 end
 
+def extract_macros
+  @macros = session[:user_one][:macros].split(',').map(&:to_i)
+  @meals = session[:user_one][:meals].to_i
+  @total_calories = session[:user_one][:calories].to_i
+end
+
+def retrieve_calories_per_gram(macros, calories)
+  @protein, @carb, @fat = macros.map.with_index do |macro, idx|
+    (macro.to_f / 100) * calories / CALORIES_PER_GRAM[MACRO[idx]]
+  end.map { |ratio| (ratio / @meals).floor(2) }
+end
+
+def retrieve_calories_per_meal
+  @calories_per_meal = @total_calories / @meals
+end
+
+def initialize_macros
+  extract_macros
+  retrieve_calories_per_meal
+  retrieve_calories_per_gram(@macros, @total_calories)
+end
+
 get '/build-meal-plan' do
+  initialize_macros
   erb :"plan.html", layout: :"layout.html"
 end
 
-get '/edit-meal:id' do
+get 'build-meal-plan/edit-meal:id' do
+  
   'edit meal'
 end
 
@@ -70,7 +97,7 @@ post '/enter-calories' do
     session[:error] = 'Enter a numeric value'
     redirect '/enter-calories'
   else
-    session['user_calories'] = params[:calories]
+    session[:user_one][:calories] = params[:calories]
     redirect 'enter-meals'
   end
 end
@@ -80,12 +107,14 @@ post '/enter-meals' do
     session[:error] = 'Enter a valid meal count'
     redirect '/enter-meals'
   else
-    session['user_meals'] = params[:meals]
+    session[:user_one][:meals] = params[:meals]
     redirect 'choose-macros'
   end
 end
 
 post '/choose-macros' do 
+  
+  session[:user_one][:macros] = params[:macros]
   redirect 'build-meal-plan'
 end
 
