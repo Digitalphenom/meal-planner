f895bbd4de193af5186da1c35d44dcb57ed3acf3
Sedrick <digitalphenom0@gmail.com>
2025-03-10 17:03:02 -0700

    added edit page
    set macro values for each meal logic
    
    added dynamic meal count assignment in meals view
    
    updated meals-button-container to meal-btn-container
    
    added css modifier to meals-btn-container (--single-meal)in edit view page

--- lib/meal_planner.rb
+++ lib/meal_planner.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
+
 require 'pry-byebug'
 require 'sinatra'
 require 'erubi'
 require 'sinatra/reloader' if development?
 require 'rack-livereload'
 
-
 set :public_folder, File.expand_path('../public', __dir__)
 set :views, File.expand_path('../views', __dir__)
-MACRO = [:protein, :carb, :fat]
-CALORIES_PER_GRAM = {protein: 4, carb: 4, fat: 9 }
+MACRO = %i[protein carb fat total_calories].freeze
+CALORIES_PER_GRAM = { protein: 4, carb: 4, fat: 9 }.freeze
 
 configure do
   use Rack::LiveReload
@@ -17,8 +17,7 @@ configure do
   set :session_secret, SecureRandom.hex(32)
 end
 
-
-helpers do 
+helpers do
   def add_inline
     @add_inline ? 'background-color: #FFB5B5' : ''
   end
@@ -26,42 +25,24 @@ helpers do
   def macros
     { Endurance: %w[20 50 30],
       Strength: %w[40 30 30],
-      Weight_Loss: %w[50 25 25] 
-    }
+      Weight_Loss: %w[50 25 25] }
   end
 end
 
+# ◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
+
 def invalid_input?(input)
-  if env["REQUEST_PATH"] == '/enter-meals'  
+  if env['REQUEST_PATH'] == '/enter-meals'
     input.to_i > 12 || input.match?(/\D/) || input.to_i.zero?
   else
     input.match?(/\D/) || input.to_i.zero?
   end
 end
 
-#◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
-
-get '/' do
-  session[:user_one] = {}
-  erb :"home.html", layout: :"layout.html"
-end
-{ "user_one"=>{:calories => 100, }}
-get '/enter-calories' do
-  erb :"calories.html", layout: :"layout.html"
-end
-
-get '/enter-meals' do
-  erb :"meals.html", layout: :"layout.html"
-end
-
-get '/choose-macros' do
-  erb :"macros.html", layout: :"layout.html"
-end
-
 def extract_macros
-  @macros = session[:user_one][:macros].split(',').map(&:to_i)
-  @meals = session[:user_one][:meals].to_i
-  @total_calories = session[:user_one][:calories].to_i
+  @macros = session[:user_one][:presets][:macros].split(',').map(&:to_i)
+  @meals = session[:user_one][:presets][:meals].to_i
+  @total_calories = session[:user_one][:presets][:calories].to_i
 end
 
 def retrieve_calories_per_gram(macros, calories)
@@ -80,24 +61,55 @@ def initialize_macros
   retrieve_calories_per_gram(@macros, @total_calories)
 end
 
+def capture_calc_values
+  [@protein, @carb, @fat, @calories_per_meal].each.with_index do |macro, i|
+    session[:user_one][:calc][MACRO[i]] = macro
+  end
+end
+
+# ◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
+
+get '/' do
+  session[:user_one] = {}
+  session[:user_one][:presets] = {}
+  session[:user_one][:calc] = {}
+  erb :"home.html", layout: :"layout.html"
+end
+
+get '/enter-calories' do
+  erb :"calories.html", layout: :"layout.html"
+end
+
+get '/enter-meals' do
+  erb :"meals.html", layout: :"layout.html"
+end
+
+get '/choose-macros' do
+  erb :"macros.html", layout: :"layout.html"
+end
+
 get '/build-meal-plan' do
   initialize_macros
+  capture_calc_values
   erb :"plan.html", layout: :"layout.html"
 end
 
-get 'build-meal-plan/edit-meal:id' do
-  
-  'edit meal'
+get '/build-meal-plan/edit-meal:id' do
+  @protein, @carb, @fat, @calories_per_meal = MACRO.map do |macro|
+    session[:user_one][:calc][macro]
+  end
+  @meal_id = params[:id].to_i + 1
+  erb :"edit-meal.html", layout: :"layout.html"
 end
 
-#◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
+# ◟◅◸◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◅▻◞
 
 post '/enter-calories' do
   if invalid_input?(params[:calories])
     session[:error] = 'Enter a numeric value'
     redirect '/enter-calories'
   else
-    session[:user_one][:calories] = params[:calories]
+    session[:user_one][:presets][:calories] = params[:calories]
     redirect 'enter-meals'
   end
 end
@@ -107,14 +119,13 @@ post '/enter-meals' do
     session[:error] = 'Enter a valid meal count'
     redirect '/enter-meals'
   else
-    session[:user_one][:meals] = params[:meals]
+    session[:user_one][:presets][:meals] = params[:meals]
     redirect 'choose-macros'
   end
 end
 
-post '/choose-macros' do 
-  
-  session[:user_one][:macros] = params[:macros]
+post '/choose-macros' do
+  session[:user_one][:presets][:macros] = params[:macros]
   redirect 'build-meal-plan'
 end
 
@@ -122,5 +133,5 @@ post '/build-meal-plan/destroy-meal:id' do
   # iterate through meals and reject :id
   # re-serve the meals page
   # redirect '/build-meal-plan'
-  "Destroy Meal"
+  'Destroy Meal'
 end
